70252d3006c62a25ebf57f25fe616b31afdf3b65
lunarok@gmail.com <lunarok@gmail.com>
2017-08-06 23:44:19 +0200

    log

--- core/class/geotrav.class.php
+++ core/class/geotrav.class.php
@@ -153,7 +153,7 @@ class geotrav extends eqLogic {
   }
 
   public static function updateGeofenceValues($id,$long1,$lat1) {
-    log::add('geotrav', 'debug', 'In update ' . $id . ' ' . $long1 . ' ' . $lat1);
+    log::add('geotrav', 'debug', 'In update ' . $this->getName() . ' ' . $id . ' ' . $long1 . ' ' . $lat1);
     $coordinate = geotravCmd::byId($id);
     $coords = explode(',',$coordinate->execCmd());
     $long2 = $coords[1];
