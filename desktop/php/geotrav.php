bbacf5cd54aa33bdc9bd3989c81e5df0d29771d2
Cédric Dupont <lunarok@gmail.com>
2017-08-30 13:58:12 +0200

    add icons per type

--- desktop/php/geotrav.php
+++ desktop/php/geotrav.php
@@ -36,29 +36,7 @@ $eqLogics = eqLogic::byType('geotrav');
                 $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
                 echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
                 echo "<center>";
-                echo '<img src="plugins/geotrav/doc/images/geotrav_icon.png" height="105" width="95" />';
-                echo "</center>";
-                echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-                echo '</div>';
-              }
-            }
-            ?>
-        </div>
-        <legend><i class="icon nature-planet5"></i> {{Localisations}}</legend>
-        <div class="eqLogicThumbnailContainer">
-            <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-                <center>
-                    <i class="fa fa-plus-circle" style="font-size : 7em;color:#00979c;"></i>
-                </center>
-                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
-            </div>
-            <?php
-            foreach ($eqLogics as $eqLogic) {
-              if ($eqLogic->getConfiguration('type') == 'location') {
-                $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-                echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-                echo "<center>";
-                echo '<img src="plugins/geotrav/doc/images/geotrav_icon.png" height="105" width="95" />';
+                echo '<img src="plugins/geotrav/doc/images/geotrav_location.png" height="105" width="95" />';
                 echo "</center>";
                 echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
                 echo '</div>';
@@ -80,7 +58,7 @@ $eqLogics = eqLogic::byType('geotrav');
                 $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
                 echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
                 echo "<center>";
-                echo '<img src="plugins/geotrav/doc/images/geotrav_icon.png" height="105" width="95" />';
+                echo '<img src="plugins/geotrav/doc/images/geotrav_travel.png" height="105" width="95" />';
                 echo "</center>";
                 echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
                 echo '</div>';
