6522193dd28eadd7a14a10e356bdbf131f185378
Cédric Dupont <lunarok@gmail.com>
2017-09-19 22:15:20 +0200

    Update geotrav.php

--- desktop/php/geotrav.php
+++ desktop/php/geotrav.php
@@ -30,6 +30,12 @@ $eqLogics = eqLogic::byType('geotrav');
               </center>
               <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
           </div>
+          <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+        <center>
+          <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+      </div>
       </div>
 
         <legend><i class="icon nature-planet5"></i> {{Localisations}}</legend>
