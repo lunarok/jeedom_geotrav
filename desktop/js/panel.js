42cca48565e4622b30c987526b148f4ebdc70438
Loïc <loic@jeedom.com>
2017-10-18 10:53:49 +0200

    add edit widget on panel

--- desktop/js/panel.js
+++ desktop/js/panel.js
@@ -15,27 +15,77 @@
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
 
- positionEqLogic();
  setTimeout(function () {
-    $('.div_displayEquipement').packery({
-        itemSelector: ".eqLogic-widget",
-        gutter : 2
-    });
-}, 2);
-
-
- $('#bt_displayObject').on('click', function () {
-    if ($(this).attr('data-display') == 1) {
-        $('#div_displayObjectList').hide();
-        $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-12 col-md-12 col-sm-12');
-        $('.div_displayEquipement').each(function () {
-            $(this).packery();
-        });
-        $(this).attr('data-display', 0);
-    } else {
-        $('#div_displayObjectList').show();
-        $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-10 col-md-9 col-sm-8');
-        $('.div_displayEquipement').packery();
-        $(this).attr('data-display', 1);
-    }
+  positionEqLogic();
+  $('.div_displayEquipement').disableSelection();
+  $( "input").click(function() { $(this).focus(); });
+  $( "textarea").click(function() { $(this).focus(); });
+  $('.div_displayEquipement').each(function(){
+    var container = $(this).packery({
+      itemSelector: ".eqLogic-widget",
+      gutter : 2,
+  });
+    var itemElems =  container.find('.eqLogic-widget');
+    itemElems.draggable();
+    container.packery( 'bindUIDraggableEvents', itemElems );
 });
+  $('.div_displayEquipement .eqLogic-widget').draggable('disable');
+  $('#bt_editDashboardWidgetOrder').on('click',function(){
+    if($(this).attr('data-mode') == 1){
+      $.hideAlert();
+      $(this).attr('data-mode',0);
+      editWidgetMode(0);
+      $(this).css('color','black');
+  }else{
+      $('#div_alert').showAlert({message: "{{Vous êtes en mode édition vous pouvez redimensionner les widgets}}", level: 'info'});
+      $(this).attr('data-mode',1);
+      editWidgetMode(1);
+      $(this).css('color','rgb(46, 176, 75)');
+  }
+});
+}, 1);
+
+ 
+function editWidgetMode(_mode){
+    if(!isset(_mode)){
+        if($('#bt_editDashboardWidgetOrder').attr('data-mode') != undefined && $('#bt_editDashboardWidgetOrder').attr('data-mode') == 1){
+            editWidgetMode(0);
+            editWidgetMode(1);
+        }
+        return;
+    }
+    if(_mode == 0){
+     if( $('.div_displayEquipement .eqLogic-widget.ui-resizable').length > 0){
+        $('.div_displayEquipement .eqLogic-widget.allowResize').resizable('destroy');
+    }
+    if( $('.div_displayEquipement .eqLogic-widget.ui-draggable').length > 0){
+       $('.div_displayEquipement .eqLogic-widget').draggable('disable');
+   }
+}else{
+ $('.div_displayEquipement .eqLogic-widget').draggable('enable');
+
+ $( ".div_displayEquipement .eqLogic-widget.allowResize").resizable({
+  grid: [ 10, 10 ],
+  resize: function( event, ui ) {
+     var el = ui.element;
+     el.closest('.div_displayEquipement').packery();
+ },
+ stop: function( event, ui ) {
+    var el = ui.element;
+    positionEqLogic(el.attr('data-eqlogic_id'));
+    el.closest('.div_displayEquipement').packery();
+    var eqLogic = {id : el.attr('data-eqlogic_id')}
+    eqLogic.display = {};
+    eqLogic.display.width =  Math.floor(el.width() / 10) * 10 + 'px';
+    eqLogic.display.height = Math.floor(el.height() / 10) * 10+ 'px';
+    jeedom.eqLogic.simpleSave({
+        eqLogic : eqLogic,
+        error: function (error) {
+            $('#div_alert').showAlert({message: error.message, level: 'danger'});
+        }
+    });
+}
+});
+}
+editWidgetCmdMode(_mode);
+}
--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -20,37 +20,38 @@ foreach (eqLogic::byType('geotrav') as $eqLogic) {
 	}
 }
 ?>
+<i class="fa fa-pencil pull-right cursor reportModeHidden" id="bt_editDashboardWidgetOrder" data-mode="0" style="margin-right : 10px;"></i>
 <?php if (count($location) > 0) {
 	?>
-<legend><i class="icon nature-planet5"></i> {{Localisations}}</legend>
-<div class="div_displayEquipement">
-	<?php
+	<legend><i class="icon nature-planet5"></i> {{Localisations}}</legend>
+	<div class="div_displayEquipement">
+		<?php
 foreach ($location as $eqLogic) {
 		echo $eqLogic->toHtml('dview');
 	}
 	?>
-</div>
-<?php }?>
-<?php if (count($travel) > 0) {
+	</div>
+	<?php }?>
+	<?php if (count($travel) > 0) {
 	?>
-<legend><i class="icon nature-planet5"></i> {{Trajets}}</legend>
-<div class="div_displayEquipement">
-	<?php
+		<legend><i class="icon nature-planet5"></i> {{Trajets}}</legend>
+		<div class="div_displayEquipement">
+			<?php
 foreach ($travel as $eqLogic) {
 		echo $eqLogic->toHtml('dview');
 	}
 	?>
-</div>
-<?php }?>
-<?php if (count($geofence) > 0) {
+		</div>
+		<?php }?>
+		<?php if (count($geofence) > 0) {
 	?>
-<legend><i class="icon nature-planet5"></i> {{Geofence}}</legend>
-<div class="div_displayEquipement">
-	<?php
+			<legend><i class="icon nature-planet5"></i> {{Geofence}}</legend>
+			<div class="div_displayEquipement">
+				<?php
 foreach ($geofence as $eqLogic) {
 		echo $eqLogic->toHtml('dview');
 	}
 	?>
-</div>
-<?php }?>
-<?php include_file('desktop', 'panel', 'js', 'geotrav');?>
+			</div>
+			<?php }?>
+			<?php include_file('desktop', 'panel', 'js', 'geotrav');?>
