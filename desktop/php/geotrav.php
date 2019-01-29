a3ab8b4b31ae6348adef3d46a0a741175ac9e99d
Cédric Dupont <lunarok@gmail.com>
2019-01-29 22:07:37 +0100

    add new types

--- desktop/php/geotrav.php
+++ desktop/php/geotrav.php
@@ -120,6 +120,44 @@ $eqLogics = eqLogic::byType('geotrav');
 			}
 			?>
 		</div>
+		<legend><i class="icon nature-planet5"></i> {{Partages Google Shared Location}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<?php
+			foreach ($eqLogics as $eqLogic) {
+				if ($eqLogic->getConfiguration('type') == 'station') {
+					$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+					echo "<center>";
+					echo '<img src="plugins/geotrav/plugin_info/geotrav_google.png" height="105" width="95" />';
+					echo "</center>";
+					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+					echo '</div>';
+				}
+			}
+			?>
+		</div>
+		<legend><i class="icon nature-planet5"></i> {{Devices iCloud}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+				<center>
+					<i class="fa fa-plus-circle" style="font-size : 7em;color:#00979c;"></i>
+				</center>
+				<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
+			</div>
+			<?php
+			foreach ($eqLogics as $eqLogic) {
+				if ($eqLogic->getConfiguration('type') == 'station') {
+					$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+					echo "<center>";
+					echo '<img src="plugins/geotrav/plugin_info/geotrav_icloud.png" height="105" width="95" />';
+					echo "</center>";
+					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+					echo '</div>';
+				}
+			}
+			?>
+		</div>
 	</div>
 
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
