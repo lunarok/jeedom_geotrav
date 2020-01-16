43f6e140605f36ec5a7c8bc09015343bf14b6d32
DUPONT <cedric.dupont@atos.net>
2020-01-16 16:34:53 +0100

    new look

--- desktop/php/geotrav.php
+++ desktop/php/geotrav.php
@@ -6,22 +6,23 @@ sendVarToJS('eqType', 'geotrav');
 $eqLogics = eqLogic::byType('geotrav');
 ?>
 <div class="row row-overflow">
-	<div class="col-lg-2 col-sm-3 col-sm-4">
-		<div class="bs-sidebar">
-			<ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-				<a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fas fa-plus-circle"></i> {{Ajouter un équipement}}</a>
-				<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-				<?php
-				foreach ($eqLogics as $eqLogic) {
-					$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-					echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"  style="' . $opacity . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
-				}
-				?>
-			</ul>
-		</div>
-	</div>
+  <div class="col-lg-2 col-sm-3 col-sm-4" id="hidCol" style="display: none;">
+    <div class="bs-sidebar">
+      <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+        <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+        <?php
+        foreach ($eqLogics as $eqLogic) {
+          echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+        }
+        ?>
+      </ul>
+    </div>
+  </div>
 
-	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+  <div class="col-lg-12 eqLogicThumbnailDisplay" id="listCol">
+  <input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+
+
 		<legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
@@ -32,8 +33,7 @@ $eqLogics = eqLogic::byType('geotrav');
 			</div>
 		</div>
 
-		<legend><i class="icon nature-planet5"></i> {{Localisations statiques et mobiles}}</legend>
-		<div class="eqLogicThumbnailContainer">
+  <legend><i class="fas fa-home" id="butCol"></i>  {{Mes Equipements}}</legend>		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
 				<center>
 					<i class="fas fa-plus-circle" style="font-size : 7em;color:#00979c;"></i>
